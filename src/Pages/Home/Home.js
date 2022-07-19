5b7ecc77d513cbf35f910c8feebee06b3574cace
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-07-20 03:22:02 +0600

    scheduling section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import GetStarted from "./GetStarted";
+import Scheduling from "./Scheduling";
 const Home = () => {
   return (
     <div>
       <GetStarted></GetStarted>
+      <Scheduling></Scheduling>
     </div>
   );
 };
