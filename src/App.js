612951810eb1db70ce56b32db2f6591a65961b5b
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-07-19 23:02:00 +0600

    Worked on Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom";
 import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2 className='text-4xl text-primary text-center'>This is our home page</h2>
-        </div>
+       <div></div>
     );
 };
 
