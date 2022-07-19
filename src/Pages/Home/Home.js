bbc4f2b0f5892da6636bb9bbd59470c1690efb22
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-07-19 17:07:41 +0600

    code moved to development branch

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>this is home page</h2>
+            <h2 className='text-4xl text-primary text-center'>This is our home page</h2>
         </div>
     );
 };
