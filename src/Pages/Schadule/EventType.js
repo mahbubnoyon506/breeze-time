b1d9e31730a0f747a3e3c2a81e4681a7cf754101
Noman01 <neamulhaque2002@gmail.com>
2022-07-28 12:19:29 +0600

    one logo added

--- src/Pages/Schadule/EventType.js
+++ src/Pages/Schadule/EventType.js
@@ -9,7 +9,7 @@ const EventType = () => {
         <div>
             <div className="flex flex-wrap sm:flex-no-wrap items-center justify-between w-full">
                 <div className="w-full sm:w-1/3 h-64 rounded-t sm:rounded-l sm:rounded-t-none shadow bg-white dark:bg-gray-800" >
-
+                    <img src={one} alt="one-to-one meeting" />
                 </div>
                 <div className="w-full sm:w-1/3 h-64 shadow bg-white dark:bg-gray-800" >
 
