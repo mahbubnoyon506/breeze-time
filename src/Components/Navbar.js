63fa6f1b4f277cb50e2ddcd0e7e5b8dd11085006
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-08-23 01:55:48 +0600

    notification updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -88,8 +88,10 @@ const Navbar = () => {
           {showNotification ? (
             <div className="">
               {notifications.map((n) => (
-                <div className="z-10 bg-info py-1 px-3 left-[-6rem] rounded relative my-2 mr">
-                  <p className=" text-sm font-mono">{n?.notification}</p>
+                <div className="z-10 bg-info py-1 px-3 left-[-5.5rem] rounded relative my-2 mr">
+                  <p className="text-black text-sm font-mono">
+                    {n?.notification}
+                  </p>
                 </div>
               ))}
             </div>
