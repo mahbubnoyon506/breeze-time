e91a535c1327775be7a36bbadd058f2cf77c1705
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-08-24 13:26:32 +0600

    model added in pricing table

--- src/Pages/Dashboard/AdminDashboard/PackageTable.js
+++ src/Pages/Dashboard/AdminDashboard/PackageTable.js
@@ -1,34 +1,23 @@
 import React from 'react';
 
-const PackageTable = ({ memberPackage, index, refetch }) => {
-    const { _id, name, price, activeEvent, calender, groupEvent, notificationStatus, oneToOne, accessType } = memberPackage;
+const PackageTable = ({ memberPackage, index, refetch, setUpdatePackage, setDeletePackage}) => {
+    const { _id, name, price } = memberPackage;
     console.log(memberPackage);
 
     return (
         <tr class="hover">
             <th>{index + 1}</th>
             <th>{name}</th>
-            <td>
-                <ul>
-                    <li>Active Event: {activeEvent}</li>
-                    <li>Calender: {calender}</li>
-                    <li>Group Event: {groupEvent}</li>
-                    <li>Notification Status: {notificationStatus}</li>
-                    <li>One-To-One: {oneToOne}</li>
-                    <li>Access Type: {accessType}</li>
-                </ul>
-            </td>
-
             <td>{
                 isNaN(price) ?
                     `${price}` :
                     `$${price}/month`
             }</td>
-            <td>
-                <button className='btn btn-outline btn-sm btn-primary'>Update Package</button>
-
+            <td>               
+                <label onClick={setUpdatePackage(memberPackage)} for="updatePackage" class="btn btn-outline btn-sm btn-primary">Update</label>
             </td>
-            <td><button className='btn btn-outline btn-sm btn-primary'>Delete Package</button>
+            <td>
+                <label onClick={setDeletePackage(memberPackage)} for="deletePackage" class="btn btn-outline btn-sm btn-primary">Delete</label>
             </td>
         </tr>
     );
